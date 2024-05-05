70921aac66ecdc33eb43fad32f56bfa6c8697fdf
goofy foot <williamk31@gmail.com>
2024-05-05 13:45:26 -0600

    implemented asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -29,5 +29,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  
+  new CacheFirst()
 );
+
+offlineFallback();
