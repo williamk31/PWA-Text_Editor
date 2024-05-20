dba25298023217f892b448e6f0b936ff089f3b46
goofy foot <williamk31@gmail.com>
2024-05-19 19:26:03 -0600

    fixed issues with offline mode

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,13 +13,13 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   try{
   console.log('PUT to the database');
   const jateDb = await openDB('jate',1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, content: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result);
   } catch (err) {
@@ -34,10 +34,10 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
   } catch (err) {
     console.error('getDb not implemented');
   }
